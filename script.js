c1c8cd0b882424efae7156c6fa91815ccd9ebf64
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-16 00:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.Name=name;
+	this.Age=age;
+	this.greet=function(){
+		console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee extends Person(name, age, jobTitle) {
+	super(name,age);
+	this.jobTitle=jobTitle;
+	this.jobGreet=function () {
+		console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old, and my job title is ${this.jobTitle}.`);
+	}
+}
+const Alice=new Person("Alice",25);
+Alice.greet;
+const Bod=new Employee("Bob",30,"Manager");
+Bob.jobGreet;
 
 // Do not change code below this line
 window.Person = Person;
